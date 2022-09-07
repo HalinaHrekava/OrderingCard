b3026b7c279c8d902eab11fdc97d42913e28af66
Halina Hrekava <gluk2019@mail.ru>
2022-09-07 14:16:02 +0300

    Build gradle changed

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -2,36 +2,64 @@ package ru.netology;
 
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CallbackTest {
 
     private WebDriver driver;
 
     @BeforeAll
-    static void setupAll() {
-        System setProperty ("webdriver.chromedriver", "driver/chromedriver.exe");
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
     }
 
+
+    //static void setupAll() {
+    // System.setProperty("webdriver.chromedriver", "E:\\Idea Projects\\Aqa projects\\Ordering a card\\driver\\chromedriver.exe");
+
+
     @BeforeEach
-    void setup();
-
-    driver =new
-
-    ChromeDriver();
-}
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
 
     @AfterEach
-    void tearDown();
-  driver.quit();
-          driver=null;
-          }
-
-@Test
-void test();
-        driver get("http://localhost:9999");
-        System.out.printIn();
-        }
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
 
 
-        }
+    @Test
+    void test() {
+
+        driver.get("http://localhost:9999");
+        List<WebElement> elements;
+        elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Александр");
+        elements.get(1).sendKeys("+37529000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+}
+
+
